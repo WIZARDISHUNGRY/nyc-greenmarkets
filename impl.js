0dd347b074a66b6bbf91e25b2c8e70a811eef5bd
Jon Williams <jonathan.williams@gmail.com>
2012-09-22 01:49:13 -0400

    visual style

--- impl.js
+++ impl.js
@@ -1,3 +1,15 @@
+(function($){
+    $.fn.extend({
+        center: function () {
+            return this.each(function() {
+                var top = ($(window).height() - $(this).outerHeight()) / 2;
+                var left = ($(window).width() - $(this).outerWidth()) / 2;
+                $(this).css({position:'absolute', margin:0, top: (top > 0 ? top : 0)+'px', left: (left > 0 ? left : 0)+'px'});
+            });
+        }
+    }); 
+})(jQuery);
+
 function Greenmarket(markets,scope) {
 	this.data=markets;
 	this.scope=scope;
@@ -5,20 +17,22 @@ function Greenmarket(markets,scope) {
 	this.initGeo=function() {
 		if ("geolocation" in navigator) {
 			//alert("geolocation is available");
-			navigator.geolocation.getCurrentPosition(
+			var val = navigator.geolocation.getCurrentPosition(
 				function(event) {self._geo(event)},
 				function(event) {self._geoError(event)}
 			);
 		}
 		else {
-			alert("I'm sorry, but geolocation services are not supported by your browser.");
+      self._showError("geo-missing");
 		}
 	}
 
 	this.initDom= function() {
-		$('.refresh').click(function() {
+		$('.refresh',this.scope).click(function() {
 			self.initGeo();
 		});
+    $('.error',this.scope).hide();
+    $(this.scope).center();
 	}
 
 	this.init= function() {
@@ -31,8 +45,9 @@ function Greenmarket(markets,scope) {
 		this._distance(position.coords.latitude, position.coords.longitude);
 	}
 
-	this._geoEror=function(error) {
-		alert("Position Error: " + positionError.message);
+	this._geoError=function(positionError) {
+		console.log("Position Error: " + positionError.message);
+    self._showError("geo-perm");
 	}
 
 	this._distance=function(latitude, longitude) {
@@ -48,11 +63,15 @@ function Greenmarket(markets,scope) {
 			return a.distance - b.distance;
 		})
 
+    $('.results',this.scope).empty();
 		for(i in data) {
 			var market = data[i];
-			this.scope.append(this._decorateItem(market));
+			$('.results',this.scope).append(this._decorateItem(market));
 		}
+    $(this.scope).center();
+    $('.results *').slideDown();
 	}
+
 	this._calcDistance=function(x1, y1, x2, y2) {
 		return Math.sqrt(
 			Math.pow(x2-x1,2) +
@@ -62,11 +81,9 @@ function Greenmarket(markets,scope) {
 	this._decorateItem=function(item) {
 		var geo = item["geo"];
 		var adr = geo["address_components"]; // this is a candidate for refactor in the Ruby
-		debugger;
 		return " \
 			<div class='vcard'> \
 				<div class='adr'> \
-					<h3>"+ item.distance +"</h3> \
 					<div class='org fn organization-name extended-address'>"+ item["Market Name"] +"</div> \
 					"+( (adr.length>5) ? " \
 						<div class='street-address'>"+ adr[0].long_name +"</div> \
@@ -80,6 +97,10 @@ function Greenmarket(markets,scope) {
 			</div> \
 		";
 	}
+
+  this._showError=function(class_name) {
+    $('.error.'+class_name,this.scope).show();
+  }
 
 	// PREAMBLE
 
--- index.md
+++ index.md
@@ -3,21 +3,17 @@ layout: default
 title: NYC Greenmarkets locator
 exclude: [ Gemfile, Gemfile.lock, LICENSE.txt, README.rdoc, Rakefile, fixtures, index.md ]
 ---
-<script src="fixtures/transformed.json"> </script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"> </script>
-<script src="impl.js"> </script>
-<script>
-window.onload = function() {
-	g = Greenmarket(markets,$("#greenmarkets"));
-	g.init();
-}
-</script>
-<style>
-.adr {
-	border: 1px;
-	padding: 12px;
-}
-</style>
-<form id="greenmarkets">
-<a href="#" class="refresh"> Refresh</a>
-</form>
+<div class="main" style="">
+  <div class="errors">
+    <div class="error geo-perm">
+      <a href="http://support.google.com/maps/bin/answer.py?hl=en&answer=153807">Please make sure location services are enabled in your browser.</a>
+    </div>
+    <div class="error geo-missing">
+      Your browser doesn't seem to support geolocation. Bummer!
+    </div>
+  </div>
+  <div class="toolbar">
+    <a href="#" class="refresh">Refresh</a>
+  </div>
+  <div class="results"> </div>
+</div>
